f0350509ec7e58908e1d87e895e8fd133ba7c310
Rafael Pereira <94644258+rafaelpssilva@users.noreply.github.com>
2022-03-01 19:08:07 -0300

    174 - Revisão #01

--- exercicios-js/node-js/projeto/src/server.js
+++ exercicios-js/node-js/projeto/src/server.js
@@ -1,13 +1,25 @@
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancoDeDados')
 
 const porta = 8080
 
+
+
 app.get('/produtos', (req, res, next) => {
-    res.send({
-        nome: 'Notebook',
-        preco: 123.45
+    res,send(bancoDeDados.getProdutos())
+})
+
+app.get('produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('./produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
     })
+    res.send(produto)
 })
 
 app.listen(porta, () => {
