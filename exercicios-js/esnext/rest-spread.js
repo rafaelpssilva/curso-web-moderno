85c86653809a00bf184b7f4166f1be8fa9ccf301
Rafael Pereira <94644258+rafaelpssilva@users.noreply.github.com>
2022-03-01 19:47:23 -0300

    177 - Operador Rest/Spread

---
+++ exercicios-js/esnext/rest-spread.js
@@ -0,0 +1,12 @@
+// operador ... rest(juntar) / spread(espalhar)
+// usar rest com parâmetro de função 
+
+// usar spread com objeto 
+const funcionario = { nome: 'Maria', salario: 12333.22 }
+const clone = { ativo: true, ...funcionario }
+console.log(clone)
+
+// usar spread com array 
+const grupoA = ['João', 'Pedro', 'Gloria']
+const grupoFinal = ['Maria', ...grupoA, 'Rafaela']
+console.log(grupoFinal)
