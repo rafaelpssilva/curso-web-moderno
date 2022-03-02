95163ca59263ed08db6eabbb80d5bdaea7dc1ece
Rafael Pereira <94644258+rafaelpssilva@users.noreply.github.com>
2022-03-02 10:17:29 -0300

    182 - For OF

--- exercicios-js/esnext/forOf.js
+++ exercicios-js/esnext/forOf.js
@@ -13,5 +13,28 @@ for (let assunto of assuntosEcma) {
 }
 
 const assuntosMap = new Map([
-    gggg
+    ['Map', {abordado: true}],
+    ['Set', {abordado: true}],
+    ['Promise', {abordado: false}]
 ])
+
+for (let assunto of assuntosMap ) {
+    console.log(assunto)
+}
+
+for(let chave of assuntosMap.keys()) {
+    console.log(chave)
+}
+
+for (let assunto of assuntosMap.values()) {
+    console.log(assunto)
+}
+
+for (let [ch, vl] of assuntosMap.entries()) {
+    console.log(ch, vl)
+}
+
+const s = new Set(['a', 'b', 'c'])
+for (let letra of s) {
+    console.log(letra)
+}
