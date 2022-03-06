ccb2ac60a6368926fd3e6a7a0b794d280bcf643c
Rafael Pereira <94644258+rafaelpssilva@users.noreply.github.com>
2022-03-06 12:52:42 -0300

    185 Refatorando Callbacks p/ Promises

---
+++ exercicios-js/esnext/usando-callbacks-promises.js
@@ -0,0 +1,33 @@
+// com promises...
+const http = require('http')
+
+const getTurma = letra => {
+    const url = `http://files.cod3r.com.br/curso-js/turma${letra}.json`
+    return new Promise((resolve, reject) => {
+        http.get(url, res => {
+            let resultado = ''
+
+            res.on('data', dados => {
+                resultado += dados
+            })
+
+            res.on('end', () => {
+                try {
+                    resolve(JSON.parse(resultado))
+                } catch (err) {
+                    reject(err)
+                }
+            })
+        })
+    })
+}
+
+let nomes = []
+
+Promise.all([getTurma('A'), getTurma('B'), getTurma('C')])
+    .then(turmas => [].concat(...turmas))
+    .then(alunos => alunos.map(aluno => aluno.nome))
+    .then(nomes  => console.log(nomes))
+    .catch(err => console.error(err.message))
+
+getTurma('D').catch(err => console.error(err.message))
